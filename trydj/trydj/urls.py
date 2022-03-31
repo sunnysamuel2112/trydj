38d8ec44317095775c42d035977e340792bc4108
sunnysamuelms <sunnysamuel2112@gmail.com>
2022-03-31 18:08:35 +0530

    added extra comments

--- trydj/trydj/urls.py
+++ trydj/trydj/urls.py
@@ -3,7 +3,7 @@
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
-Function views
+Function views are as follows
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
